4be8cee30fa375dc3d7c6054cd05db5d52a19d81
Tsvetozar Bonev <tsbonev@gmail.com>
2018-06-14 17:05:23 +0300

    Implement repos and UoW

---
+++ Repositories/EventRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace Repositories
+{
+    public class EventRepository : BaseRepository<Event>
+    {
+
+        public EventRepository(EventManagerEntities context)
+            : base(context)
+        {
+        }
+
+        public override void Save(Event item)
+        {
+            if(item.ID == 0)
+            {
+                base.Create(item);
+            }
+            else
+            {
+                base.Update(item, e => e.ID == item.ID);
+            }
+        }
+    }
+}
---
+++ Repositories/UnitOfWork.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class UnitOfWork : IDisposable
+    {
+        private EventManagerEntities context = new EventManagerEntities();
+
+        public int Save()
+        {
+            return context.SaveChanges();
+        }
+
+        private EventRepository eventRepository;
+
+        public EventRepository EventRepository
+        {
+            get
+            {
+                if (this.eventRepository == null)
+                {
+                    this.eventRepository = new EventRepository(context);
+                }
+                return eventRepository;
+            }
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
