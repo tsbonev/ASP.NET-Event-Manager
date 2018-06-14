00b3dcb28a5a99244bd6dc9ed962521983136c85
Tsvetozar Bonev <tsbonev@gmail.com>
2018-06-14 17:00:12 +0300

    Add DataAcces and Repositories

---
+++ EventManagerDB/dbo/Tables/Event.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Event] (
+    [ID]        INT            IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (200) NOT NULL,
+    [Location]  NVARCHAR (200) NOT NULL,
+    [StartDate] DATETIME       NOT NULL,
+    [EndDate]   DATETIME       NOT NULL,
+    CONSTRAINT [PK_Events] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
